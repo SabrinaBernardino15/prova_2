67b05ffc489be46d5cff87c3201938447819980f
SabrinaBernardino15 <113374291+SabrinaBernardino15@users.noreply.github.com>
2022-12-01 21:04:11 -0300

    Add files via upload
    projeto

---
+++ cabecalho.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simulado</title>
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand " href="#" >Vestibular</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./index.php">Início</a>
+      </form>
+    </div>
+  </div>
+</nav>
+<main class="container">
+    <div class="row">
+        <div class="col-md-9 offset-md-2 col-sm-9"> <h1>Questões</h1>
+            <form action="resultado.php" method="post" onsubmit="return verificar()" >
+            <div class='card mt-10 ml-9'><div class=''><strong>
+
+            <div class="card border-dark bg-secondary mb-10" style="max-width: 100rem;">
+           <div class="card-header"></div>
+           <div class="card-body text-white">
+              <h5 class="card-title"></h5>
+
+             
+
+          
+
+<script src="./js/bootstrap.bundle.min.js" ></script> 
+<script>
+  function verificar(){
+    return true;
+  }
+</script>
+
+</body>
+</html>
